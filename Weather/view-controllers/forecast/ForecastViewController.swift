d26f0763a5f36a42ac18876e18b9de0acfdc613d
Matheus Holanda <matheus.albuquerque@agilecontent.com>
2020-08-22 16:33:51 -0300

    TabBar added. Storyboard removed.

---
+++ Weather/view-controllers/forecast/ForecastViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ForecastViewController.swift
+//  Weather
+//
+//  Created by Matheus Holanda Pinheiro de Albuquerque on 22/08/20.
+//  Copyright © 2020 Matheus Holanda. All rights reserved.
+//
+
+import UIKit
+
+class ForecastViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
